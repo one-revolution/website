31cc3b3f2799667fd5e970b3ccceff928085e3da
Gary Woodfine <lnb0l9dc@duck.com>
2025-11-20 11:30:41 +0000

    Removed `pnpm-lock.yaml` file to clean up unused dependencies.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,45 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: {enabled: true},
+    content: {
+        build: {
+            markdown: {
+                highlight: {
+                    theme: {
+                        default: 'github-light',
+                        dark: 'monokai',
+                        sepia: 'monokai'
+                    },
+                    langs: [
+                        'xml',
+                        'csharp',
+                        'typescript',
+                        'javascript',
+                        'yaml',
+                        'shell',
+                        'rust',
+                        'go',
+                        'python',
+                        'terraform',
+                        'json'
+                    ]
+                }
+            }
+        },
+        experimental: {nativeSqlite: true},
 
+    },
     modules: [
-        '@nuxt/content',
         '@nuxt/image',
         '@nuxt/scripts',
         '@nuxt/test-utils',
-        '@nuxt/ui'
+        '@nuxt/ui',
+        '@nuxt/fonts',
     ],
+    fonts: {
+        providers: {
+            bunny: false
+        }
+    },
     css: ['~/assets/css/main.css'],
 })
